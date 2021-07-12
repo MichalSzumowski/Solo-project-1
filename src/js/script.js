ddd2b9f227d99d130123f98a980324640730a129
Michał Szumowski <mic.szumowski@gmail.com>
2021-07-12 23:49:36 +0200

    Add Stats and Links sections - still missing on Links styling and finishing modals

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,15 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 
-/* TOGGLE MENU */
+/* toggle menu */
 
 const hamburger = document.querySelector('#hamburger');
 
 hamburger.addEventListener('click', function () {
-  document.getElementById('sidebar').classList.toggle('active');
-  document.getElementById('main').classList.toggle('active');
+  document.querySelector('.sidebar').classList.toggle('active');
+  document.querySelector('.main').classList.toggle('active');
 });
+
 
 /* Modals */ 
 
@@ -58,3 +59,42 @@ for (let e of modalButtons) {
     openModal(modal);
   });
 }
+
+
+/* MyCharts */
+
+const ctx = document.getElementById('myChart').getContext('2d');
+
+
+const chart = new Chart(ctx, {
+  // 1
+  type: 'bar',
+  data: {
+    // 2
+    labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'],
+    // 3
+    datasets: [{
+      // 4
+      label: 'Signups',
+      // 5
+      backgroundColor: '#8DBEC8',
+      borderColor: '#8DBEC8',
+      // 6
+      data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
+    },
+    {
+      label: 'FTD',
+      backgroundColor: '#F29E4E',
+      borderColor: '#F29E4E',
+      data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
+    },
+    {
+      label: 'Earned',
+      backgroundColor: '#71B374',
+      borderColor: '#71B374',
+      data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
+      // 7
+      hidden: true,
+    }]
+  },
+});
